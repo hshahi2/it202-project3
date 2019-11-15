5725d5d500d30466136f18d76f814f967a485930
Hamza Shahid <hshahi2@uic.edu>
2019-11-15 06:46:47 +0000

    added an object as a player

--- canvas.js
+++ canvas.js
@@ -1,13 +1,5 @@
 var background = new Image();
-background.src = "hs-2014-27-a-print.0.0.jpg";
-
-// var myC = document.getElementById("animationCanvas");
-// var ccc = myC.getContext("2d");
-// ccc.font = "30px Arial";
-// ccc.fillStyle = "yellow";
-// ccc.fillText("Hello World", 10, 50);
-               
-
+background.src = "dark_scary.png";
 
 //
 // randomly generate coordinate for the harm and heal objects 
@@ -15,12 +7,13 @@ background.src = "hs-2014-27-a-print.0.0.jpg";
 var randHeal = Math.floor((Math.random() * 400) + 50);
 var randHarm = Math.floor((Math.random() * 400) + 50);
 
-
-//                 var healX = 0, healY = randHeal;
-//                 var harmX = 0, harmY = randHarm;
-
+//
+// have that randomly generated number as the starting point 
+// for where the objects are going to come from 
+// 
 var healX = randHeal, healY = 0;
 var harmX = randHarm, harmY = 0;
+var playerX = 200, playerY = 400;
 
 var c = document.getElementById("animationCanvas"); 
 var cc = document.getElementById("animationCanvas");
@@ -30,12 +23,11 @@ var ctx = document.getElementById("animationCanvas").getContext("2d");
 var healObject = c.getContext("2d");
 healObject.fillStyle = 'green';
 
-var harmObject = cc.getContext("2d"); ///////////////
-harmObject.fillStyle = 'red';////////////
+var harmObject = cc.getContext("2d"); 
+harmObject.fillStyle = 'red';
 
 console.log(c.width, c.height);
-console.log(cc.width, cc.height);//////
-
+console.log(cc.width, cc.height);
 console.log(ctx.width, ctx.height);
 
 
@@ -57,7 +49,7 @@ function draw() {
         healY -= c.width;
     }
 
-      if (harmY > cc.width) {   ////////////////
+      if (harmY > cc.width) {   
         harmY -= cc.width;
     }
 
@@ -66,7 +58,7 @@ function draw() {
     // draw the background image
     // 
     healObject.drawImage(background, 0, 0);
-    harmObject.drawImage(background, 0, 0);///////
+    harmObject.drawImage(background, 0, 0);
     
     
     //
@@ -94,7 +86,37 @@ function draw() {
     var LEVEL = 1;
     myText.font = "25px Arial";
     myText.fillStyle = "yellow";
-    myText.fillText("LeveL: " + LEVEL, 200, 50);
+    myText.fillText("Level: " + LEVEL, 200, 50);
+    
+    
+    
+    var myCan = document.querySelector("#animationCanvas");
+    var mycontext = myCan.getContext("2d");
+    
+    function drawTriangle() {
+        mycontext.beginPath();
+        mycontext.moveTo(200, 100);
+        mycontext.lineTo(170, 150);
+        mycontext.lineTo(230, 150);
+        mycontext.closePath();
+//         myContext.translate(200,200);
+
+
+        // the outline
+        mycontext.lineWidth = 10;
+        mycontext.strokeStyle = "rgba(102, 102, 102, 1)";
+        mycontext.stroke();
+
+        // the fill color
+        mycontext.fillStyle = "rgba(255, 204, 0, 1)";
+        mycontext.fill();
+    }
+    
+    drawTriangle();
+    
+    
+    
+
     
     
 
@@ -104,8 +126,9 @@ function draw() {
     //  comment out begin and close path to see what happens
     //  
     healObject.beginPath();
-    harmObject.beginPath();//////////
-
+    harmObject.beginPath();
+    
+    
    //
    //  a square at the coordinates     
    //            
@@ -118,7 +141,7 @@ function draw() {
    //
    // a circle at the coordinates 
    // 
-   harmObject.arc(harmX, harmY, 18, 0, Math.PI * 2);              ///////
+   harmObject.arc(harmX, harmY, 18, 0, Math.PI * 2);              
    harmObject.closePath();
    harmObject.fill();
 
@@ -133,3 +156,69 @@ window.requestAnimationFrame(draw);
 
 
 
+
+
+
+
+
+
+//     var myCan = document.querySelector("#animationCanvas");
+// var mycontext = myCan.getContext("2d");
+ 
+// function drawTriangle() {
+//       mycontext.clearRect(0, 0, canvas.width, canvas.height);
+
+//   // the triangle
+//   mycontext.beginPath();
+//     mycontext.moveTo(200 + deltaX, 100 + deltaY);
+//   mycontext.lineTo(170 + deltaX, 150 + deltaY);
+//   mycontext.lineTo(230 + deltaX, 150 + deltaY);
+    
+// //   mycontext.moveTo(200, 100);
+// //   mycontext.lineTo(170, 150);
+// //   mycontext.lineTo(230, 150);
+//   mycontext.closePath();
+ 
+//   // the outline
+//   mycontext.lineWidth = 10;
+//   mycontext.strokeStyle = "rgba(102, 102, 102, 1)";
+//   mycontext.stroke();
+ 
+//   // the fill color
+//   mycontext.fillStyle = "rgba(255, 204, 0, 1)";
+//   mycontext.fill();
+// }
+    
+    
+    
+//     window.addEventListener("keydown", moveSomething, false);
+  
+    
+//     var deltaX = 0;
+// var deltaY = 0;
+    
+// function moveSomething(e) {
+//     switch(e.keyCode) {
+//         case 37:
+//             deltaX -= 2;
+//             break;
+//         case 38:
+//             deltaY -= 2;
+//             break;
+//         case 39:
+//             deltaX += 2;
+//             break;
+//         case 40:
+//             deltaY += 2;
+//             break;
+//     } 
+//         e.preventDefault();
+//     drawTriangle();
+
+
+// }  
+    
+//     function keysReleased(e) {
+//     // mark keys that were released
+//     keys[e.keyCode] = false;
+// }
